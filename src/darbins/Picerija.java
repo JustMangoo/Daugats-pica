cb7e495d9f6c885ca4e70ddfa2c574bf1cdc2a3e
Aleksis Daugats <daugatsa@gmail.com>
2022-01-27 18:34:29 +0200

    branch tests

--- src/darbins/Picerija.java
+++ src/darbins/Picerija.java
@@ -1,9 +1,36 @@
 package darbins;
 
+import javax.swing.JOptionPane;
+
 public class Picerija {
 
 	public static void main(String[] args) {
+		String izvele;
 		
+		do {
+			izvele = JOptionPane.showInputDialog("1-izveidot pasutijumu |2-skatit klientus |3-skatit picas |stop-apturet");
+			izvele = izvele.toLowerCase();
+			
+			switch(izvele){
+				case "1":
+					
+					break;
+				
+				case "2":
+					break;
+					
+				case "3":
+					break;
+					
+				case "stop":
+					JOptionPane.showMessageDialog(null, "Programma aptureta!", "Bridinajums!", JOptionPane.WARNING_MESSAGE );
+					break;
+				
+				default:
+					JOptionPane.showMessageDialog(null, "Darbiba nepastav!", "Kluda!", JOptionPane.ERROR_MESSAGE );
+					
+			}
+		}while(!izvele.equals("stop"));
 		
 
 	}
